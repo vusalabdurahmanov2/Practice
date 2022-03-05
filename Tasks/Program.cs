0e4814c497a9469194374d2f26e85869c783050b
Vusal Abdurahmanov <tu7eay65k@aztu.local>
2022-03-05 14:00:30 +0400

    added file

---
+++ Tasks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //    int[] arr = { 1, 0, 3, 4, -5, 6, 7 };//arrayimiz
+
+            //// Unsorted array
+            //for (int i = 0; i < arr.Length - 1; i++)
+            //{
+            //    // Find minumu element in array
+            //    int min_idx = i;
+            //        for (int j = i + 1; j < arr.Length; j++)
+            //        {
+            //            if (arr[j] < arr[min_idx])
+            //                min_idx = j;
+            //            // Swap found minimum element to first
+            //            int temp = arr[min_idx];
+            //            arr[min_idx] = arr[i];
+            //            arr[i] = temp;
+            //        }
+            //}Console.WriteLine(arr[0]);//Print first element because sorteder array first element is minumum 
+            #endregion
+            #region task 2
+            //string name = Console.ReadLine();//Input string
+            //char[] arr = name.ToCharArray();//Change array to char 
+            //foreach (char item in arr)//Check each letter 
+            //{
+            //    if (item == 'a')
+            //    {
+            //        Console.WriteLine("there is a letter in the text");
+            //        break;
+            //    }
+            //    else Console.WriteLine("there is't letter in the text"); break;
+            //}
+            #endregion
+        }
+
+
+    }
+}
